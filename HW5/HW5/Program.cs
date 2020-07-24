3e2faae76a128490c8cb4c698a92a286a2778ab6
feron <user.onelas@gmail.com>
2020-07-24 18:22:57 +0500

    HW5Up

---
+++ HW5/HW5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace HW5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter login: ");
+            Console.WriteLine(LoginCheck(Console.ReadLine()));
+
+            static bool LoginCheck(string login)
+            {
+                Regex reg = new Regex(@"[a-zA-Z]{1}[a-zA-Z0-9]{1,9}");
+                if (login.Length > 10)
+                    return false;
+                return reg.IsMatch(login);
+            }
+    }
+}
---
+++ Homework4/Homework4/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ClassForArray.ArrGen(20);
+            /*try
+            {
+                int[] array = ClassForArray.GetArrFromFile(@"C:\array.txt");
+                foreach(var num in array)
+                {
+                    Console.WriteLine($"{num}");
+                }
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("File not found.");
+            }*/
+            int[] MyArr = ClassForArray.ArrConstruct(10, 1, 1);
+            foreach(var i in MyArr)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine($"\n{ClassForArray.Sum(MyArr)}");
+
+
+        }
+        class ClassForArray
+        {
+            int[] MyArray;
+            public object Get { get; private set; }
+
+            public static int[] GetArrFromFile(string path)
+            {
+                string[] newArr = File.ReadAllLines(path);
+                int[] temp = new int[newArr.Length];
+                for (int i = 0; i < newArr.Length; i++)
+                {
+                    temp[i] = Convert.ToInt32(newArr[i]);
+                }
+                return temp;
+
+            }
+            public static void ArrGen(int msize)
+            {
+                int count = 0;
+                int[] MyArray = new int[msize];
+                Random rnd = new Random();
+                int i;
+                for (i = 0; i < MyArray.Length; i++)
+                {
+                    MyArray[i] = rnd.Next(-10000, 10001);
+                    Console.WriteLine($"{MyArray[i]}");
+
+                }
+                ClassForArray.ArrayDivide(MyArray);
+            }
+            public static void ArrayDivide(params int[] MyArray)
+            {
+                int count = 0;
+                int i;
+                for (i = 0; i < MyArray.Length - 1; i++)
+                {
+                    int curValue = MyArray[i];
+                    int nextValue = MyArray[i + 1];
+
+                    if ((curValue % 3 == 0) && (nextValue % 3 != 0))
+                    {
+                        count = count + 1;
+                        Console.WriteLine($"\n{curValue} {nextValue}");
+                    }
+                }
+
+                Console.WriteLine($"Total pair with only one number % 3 = {count}");
+            }
+
+            public static int[] ArrConstruct(int i, int sValue, int step)
+            {
+                int[] arr = new int[i];
+                arr[0] = sValue;
+                for (int j = 1; j < i; j++)
+                {
+                    arr[j] = arr[j - 1] + step;
+                }
+                return arr;
+            }
+            public static int Sum(int[] mass)
+            {
+                int sum = 0;
+                for (int i = 0; i < mass.Length; i++) 
+                {
+                    sum += mass[i];     
+                }
+                return sum;
+            }
+                
+             
+
+        }
+    }
+}
